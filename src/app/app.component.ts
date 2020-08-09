a05fadb4fd1d5d95304d8b738145e6713570136c
iorlovsky <i.gorstkin@deliveryweb.ru>
2020-08-10 00:12:01 +0300

    feat: Add navigation to todo-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+import { AppRoutes } from './constants/routes';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title: string = 'testing-playground';
+  readonly todoListRoute: string;
+
+  constructor() {
+    this.todoListRoute = AppRoutes.TodoList.join('');
+  }
+
 }
