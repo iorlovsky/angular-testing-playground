48e11ed807a9bbfaeb06a0339a4eb2e54152743b
iorlovsky <i.gorstkin@deliveryweb.ru>
2020-08-09 22:25:04 +0300

    style: Prettify code

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,7 @@
 // This file is required by karma.conf.js and loads recursively all the .spec and framework files
 
+// The 'zone-testing' import should be first
+/* tslint:disable:ordered-imports */
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
