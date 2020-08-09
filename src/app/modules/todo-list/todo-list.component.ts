1ef74192ca31524b2fb99a2df408ebdddf59338b
iorlovsky <i.gorstkin@deliveryweb.ru>
2020-08-09 23:50:39 +0300

    feat: Create TodoListModule

---
+++ src/app/modules/todo-list/todo-list.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TodoListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
