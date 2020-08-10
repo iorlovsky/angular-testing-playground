440431880649cbebb6f1499542726c9b8fa8ce92
iorlovsky <i.gorstkin@deliveryweb.ru>
2020-08-10 23:40:32 +0300

    refactor: Remove redundant enum

--- src/app/modules/todo-list/todo.actions.ts
+++ src/app/modules/todo-list/todo.actions.ts
@@ -2,11 +2,7 @@ import { createAction, props } from '@ngrx/store';
 
 import { Todo } from './types';
 
-export enum TodoActionTypes {
-  Create = '[Todo Page] Create Todo'
-}
-
 export const createTodo = createAction(
-  TodoActionTypes.Create,
+  '[Todo Page] Create Todo',
   props<Todo>()
 );
