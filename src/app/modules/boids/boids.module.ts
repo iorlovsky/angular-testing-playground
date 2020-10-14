ff7abccbbfb84cb83d723a0eef7f0af35deaf8a2
iorlovsky <i.gorstkin@deliveryweb.ru>
2020-10-14 17:34:02 +0300

    feat: Generate GridComponent

--- src/app/modules/boids/boids.module.ts
+++ src/app/modules/boids/boids.module.ts
@@ -5,9 +5,10 @@ import { SharedModule } from '../shared/shared.module';
 import { BoidsRoutingModule } from './boids-routing.module';
 import { BoidsComponent } from './boids.component';
 import { BoidComponent } from './components/boid/boid.component';
+import { GridComponent } from './components/grid/grid.component';
 
 @NgModule({
-  declarations: [BoidsComponent, BoidComponent],
+  declarations: [BoidsComponent, BoidComponent, GridComponent],
   imports: [
     CommonModule,
     BoidsRoutingModule,
---
+++ src/app/modules/boids/components/grid/grid.component.ts
@@ -0,0 +1,16 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-grid',
+  templateUrl: './grid.component.html',
+  styleUrls: ['./grid.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class GridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
